ae0bdbcb5b760bf46babb3cd9ccb612e7bdd00b1
Laurent Curau <laurentcurau@gmail.com>
2018-11-26 11:29:25 +0000

    Initial commit

---
+++ spec/clj_ttt/core_spec.clj
@@ -0,0 +1,7 @@
+(ns clj-ttt.core-spec
+  (:require [speclj.core :refer :all]
+            [clj-ttt.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/clj_ttt/core.clj
@@ -0,0 +1,5 @@
+(ns clj-ttt.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
