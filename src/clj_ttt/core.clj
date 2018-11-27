64f5b3bbb6025b28dde6c51e1afb106963667981
Laurent Curau <laurentcurau@gmail.com>
2018-11-27 22:05:57 +0000

    Mark private functions as such and reorder

--- src/clj_ttt/core.clj
+++ src/clj_ttt/core.clj
@@ -4,52 +4,52 @@
   [& args]
   (println "Hello World"))
 
-(def new-board ["" "" "" "" "" "" "" "" ""])
-
-(defn create-board [& cells]
-  (vector cells))
-
-(defn available? [cell]
+(defn- available? [cell]
   (clojure.string/blank? cell))
 
-(defn mark-board [board position mark]
-  (assoc board (- position 1)  mark))
+(defn- full? [board]
+  (not-any? #(= "" %) board))
+
+(defn- rows [board]
+  (apply partition 3 board))
+
+(defn- columns [board]
+  (apply mapv vector (rows board)))
+
+(defn- left-diagonal [board]
+  (map-indexed
+   (fn [row-index row] (nth row row-index)) (rows board)))
+
+(defn- right-diagonal [board]
+  (map-indexed
+   (fn [row-index row] (nth row row-index)) (reverse (rows board))))
+
+(defn- combinations [board]
+  ((juxt rows columns right-diagonal left-diagonal) board))
+
+(defn- winning-combination? [combination]
+  (apply = combination))
+
+(defn empty? [cells]
+  (every? available? cells))
 
 (defn get-cell [board position]
   (let [index (- position 1)]
     (nth board index)))
 
-(defn full? [board]
-  (not-any? #(= "" %) board))
-
-(defn empty? [cells]
-  (every? available? cells))
-
-(defn rows [board]
-  (apply partition 3 board))
-
-(defn columns [board]
-  (apply mapv vector (rows board)))
-
-(defn left-diagonal [board]
-  (map-indexed
-   (fn [row-index row] (nth row row-index)) (rows board)))
-
-(defn right-diagonal [board]
-  (map-indexed
-   (fn [row-index row] (nth row row-index)) (reverse (rows board))))
-
-(defn combinations [board]
-  ((juxt rows columns right-diagonal left-diagonal) board))
-
-(defn winning-combination? [combination]
-  (apply = combination))
+(defn no-win? [board]
+  (not-any? winning-combination? (combinations board)))
 
 (defn win? [board]
   (some winning-combination? (partition 3 (flatten (combinations board)))))
 
-(defn no-win? [board]
-  (not-any? winning-combination? (combinations board)))
-
 (defn tie? [board]
   ((every-pred full? no-win?) board))
+
+(defn mark-board [board position mark]
+  (assoc board (- position 1)  mark))
+
+(defn create-board [& cells]
+  (vector cells))
+
+(def new-board ["" "" "" "" "" "" "" "" ""])
