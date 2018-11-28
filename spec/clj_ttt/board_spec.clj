a34baeda7004dc7ae39632a8878c988500dd816f
Laurent Curau <laurentcurau@gmail.com>
2018-11-28 12:06:47 +0000

    Move board logic to own files

---
+++ spec/clj_ttt/board_spec.clj
@@ -0,0 +1,36 @@
+(ns clj-ttt.board-spec
+  (:require [speclj.core :refer :all]
+            [clj-ttt.board :refer :all]))
+
+(describe "the board"
+  (it "should be empty"
+    (should= true (pristine? new-board)))
+
+  (it "can be marked"
+    (let [board (mark-board new-board 1 "X")
+          cell (get-cell board 1)]
+      (should= "X" cell)))
+
+  (it "recognizes a diagonal win"
+    (let [board (create-board "X" "O" "X"
+                              "O" "X" "O"
+                              "O" "X" "X")]
+      (should= true (win? board))))
+
+  (it "recognizes a row win"
+      (let [board (create-board "X" "X" "X"
+                                "X" "O" "O"
+                                "O" "X" "X")]
+        (should= true (win? board))))
+
+  (it "recognizes a column win"
+      (let [board (create-board "X" "O" "X"
+                                "X" "O" "O"
+                                "X" "X" "X")]
+        (should= true (win? board))))
+
+  (it "recognizes a tie"
+      (let [board (create-board "X" "X" "O"
+                                "O" "O" "X"
+                                "X" "O" "X")]
+        (should= true (tie? board)))))
